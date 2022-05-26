96cbfbbe92996c2d4fd0fee3a2db8866269b5678
santicascallar <101258956+santicascallar@users.noreply.github.com>
2022-05-26 12:16:40 -0300

    Add files via upload

--- TP2_EFSI/hola.js
+++ TP2_EFSI/hola.js
@@ -1,8 +1,12 @@
 //https://webdesign.tutsplus.com/es/tutorials/to-do-app-with-vanilla-javascript--cms-35258
+//hacer un objeto tarea que tenga (id,titulo y estado) --> el estado siempre en false y cuando la se marque en true
 
-let contador = 0;
-let tarea = document.querySelector('input[type="text"]');
+let listaTarea = document.querySelector('input[type="text"]');
 let vacio = document.getElementById("vacio");
+let contador = 0;
+let tarea = {
+
+}
 
 formulario.addEventListener('submit', (evento)=>{
     //evento = preventDefault();
@@ -29,7 +33,9 @@ let agregarTarea = () => {
 formulario.addEventListener('click', (evento)=>{
     let check = document.querySelectorAll('input type=["checkbox"]:checked');
     if(evento.srcElement.nodeName == 'INPUT'){
-        
+    }
+    if(evento.srcElement.nodeName == 'IMG'){
+        eliminarTarea(evento.srcElement.parentNode,id);
     }
 })
 
